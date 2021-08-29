c7c518b6b3e336948c7886c6ebe95d211e8920e4
Scopes <seif.wessam66@gmail.com>
2021-08-29 19:53:41 +0200

    create /source

--- cogs/utilities.py
+++ cogs/utilities.py
@@ -2,18 +2,24 @@ from dislash import *
 import discord
 from discord.ext import commands
 
+
 class Utilities(commands.Cog):
-    
+
     def __init__(self, bot):
         self.bot = bot
-        
+
     @slash_command(name="invite", description="Get the invite link to this server.")
     async def invite(self, inter):
         await inter.respond("https://discord.gg/DaR54pXmJU")
-        
+
     @slash_command(name="ping", description="Get the ping of the bot.")
     async def ping(self, inter):
         await inter.respond(f"Running on `{round(self.bot.latency*1000)}ms`.", ephemeral=True)
-        
+
+    @slash_command(name="source", description="Link to my source code!")
+    async def source(self, inter):
+        await inter.respond("https://github.com/ScopesCodez/Soly-Bot")
+
+
 def setup(bot):
     bot.add_cog(Utilities(bot))
