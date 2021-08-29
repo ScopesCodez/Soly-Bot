dd14e968e2628d40f8e30cef9d6c42a2f248ca5c
Scopes <seif.wessam66@gmail.com>
2021-08-29 19:08:31 +0200

    added /create_tag

--- cogs/tags.py
+++ cogs/tags.py
@@ -6,14 +6,14 @@ import json
 
 
 async def load_tags_list():
-    with open("tags.json", "r") as f:
+    with open("jsons/tags.json", "r") as f:
         load = json.load(f)
 
     return load.keys()
 
 
 async def get_tag(tag):
-    with open("tags.json", "r") as f:
+    with open("jsons/tags.json", "r") as f:
         load = json.load(f)
 
     try:
@@ -22,6 +22,22 @@ async def get_tag(tag):
         response = "not found"
 
     return response
+
+
+async def create_tag(tag, response):
+    with open("jsons/tags.json", "r") as f:
+        load = json.load(f)
+
+    result = await get_tag(tag)
+    if result != "not found":
+        return "exists"
+
+    load[tag] = response
+
+    with open("tags.json", "w") as f:
+        json.dump(load, f, indent=4)
+
+    return "created"
 
 
 class Tags(commands.Cog):
@@ -58,6 +74,18 @@ class Tags(commands.Cog):
                               title="Tags", description="\n".join(tags))
 
         await inter.respond(embed=embed, ephemeral=True)
+
+    @slash_command(name="create_tag", description="Create a new tag.", options=[
+        Option("tag", "The tag's title.", Type.STRING, True),
+        Option("response", "The tag's response.", Type.STRING, True)
+    ])
+    @commands.has_permissions(administrator=True)
+    async def create_tag(self, inter, tag, response):
+        result = await create_tag(tag.lower(), response)
+        if result == "exists":
+            return await inter.respond("This tag already exists!", ephemeral=True)
+        else:
+            await inter.respond(f'Tag "**{tag}**" created!')
 
 
 def setup(bot):
