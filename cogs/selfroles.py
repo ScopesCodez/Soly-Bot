408b20e58fbc2aa3f3c83b1b1a54ecd2ba5a309b
Scopes <seif.wessam66@gmail.com>
2021-09-01 13:06:52 +0200

    created `/removerole` that removes from you roles you got with `/getrole`

--- cogs/selfroles.py
+++ cogs/selfroles.py
@@ -34,6 +34,31 @@ class SelfRoles(commands.Cog):
             await inter.author.add_roles(role)
             await inter.reply(f"I gave you the role **{role.name}**!")
 
+    @slash_command(
+        name="removerole",
+        description="Remove your self assigned roles!",
+        options=[
+            Option(name="role", description="The role you want to remove.",
+                   choices=[
+                       OptionChoice(name="partnership-ping",
+                                    value=882578885684899850),
+                       OptionChoice(name="vote-reminder",
+                                    value=881274995312037909),
+                       OptionChoice(name="supdates",
+                                    value=881275063041654845)
+                   ],
+                   required=True
+                   )
+        ]
+    )
+    async def removerole(self, inter, role):
+        role = inter.guild.get_role(role)
+        if role not in inter.author.roles:
+            return await inter.reply(f"You don't have the role **{role.name}**! If you want to get it, use `/getrole {role.name.lower()}`.", ephemeral=True)
+        else:
+            await inter.author.remove_roles(role)
+            await inter.reply(f"I removed the role **{role.name}** from you!")
+
 
 def setup(bot):
     bot.add_cog(SelfRoles(bot))
