f09a99230ec31f84ed0b71227b90f3a2797f1b38
null <60427892+vierofernando@users.noreply.github.com>
2021-10-12 16:19:21 +0700

    refactor(bot): use slices instead of replace

--- bot.py
+++ bot.py
@@ -21,11 +21,11 @@ async def on_ready():
     for cog in os.listdir(r"cogs"):
         if cog.endswith(".py"):
             try:
-                cog = f"cogs.{cog.replace('.py', '')}"
+                cog = f"cogs.{cog[:-3]}"
                 bot.load_extension(cog)
                 print(f"Loaded extension: {cog}")
             except Exception as e:
-                print(f"{cog} is failed to load:")
+                print(f"{cog} failed to load:")
                 raise e
 
     print(f"Logged in as {bot.user}")
